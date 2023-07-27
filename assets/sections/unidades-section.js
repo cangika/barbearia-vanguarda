50c38d625cba4be8c0a37aa3da7b0a89a6509616
cangica <giovanacoutorios@gmail.com>
2023-07-27 11:56:18 -0300

    add slider unidades section

--- assets/sections/unidades-section.js
+++ assets/sections/unidades-section.js
@@ -8,7 +8,7 @@ export class UnidadesSection extends LitElement {
             :host {
                 height: fit-content;
                 gap: 32px;
-                padding-bottom: 4rem;
+                padding: 0 0 4rem 0;
             }
 
             app-paragrafo {
@@ -28,6 +28,22 @@ export class UnidadesSection extends LitElement {
                 left: -64px;
                 top: -56px;
                 z-index: -10;
+            }
+
+            swiper-container {
+                width: 100%;
+                height: 300px;
+            }
+
+            swiper-slide {
+                padding-top: 1rem;
+                display: flex;
+                justify-content: center;
+                transition: transform 500ms;
+            }
+
+            .swiper-slide-active {
+                transform: scale(1.1);
             }
 
             @media (min-width: 768px) {
@@ -50,7 +66,21 @@ export class UnidadesSection extends LitElement {
         <app-paragrafo>
         Nossa sede, localizada na zona leste de São Paulo, e as demais unidades, estão abertas todos os dias. <br> <br> Confira os horários específicos de cada unidade.
         </app-paragrafo>
-        <app-mapa></app-mapa>
+        
+        <swiper-container space-between="-60">
+            <swiper-slide>
+                <app-mapa></app-mapa>
+            </swiper-slide>
+            <swiper-slide>
+                <app-mapa></app-mapa>
+            </swiper-slide>
+            <swiper-slide>
+                <app-mapa></app-mapa>
+            </swiper-slide>
+            <swiper-slide>
+                <app-mapa></app-mapa>
+            </swiper-slide>
+        </swiper-container>
         <app-bolinhas formato="metade"></app-bolinhas>
         `;
     }
