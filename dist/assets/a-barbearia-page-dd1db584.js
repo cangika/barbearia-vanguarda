e49a615b1995bbe32734631349d070c244e6f659
cangica <giovanacoutorios@gmail.com>
2023-07-28 11:37:55 -0300

    add lozad e otimização

---
+++ dist/assets/a-barbearia-page-dd1db584.js
@@ -0,0 +1,5 @@
+var i=Object.defineProperty;var n=(a,e,r)=>e in a?i(a,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):a[e]=r;var s=(a,e,r)=>(n(a,typeof e!="symbol"?e+"":e,r),r);import{s as o,i as b,x as c}from"./index-49beb27f.js";class t extends o{render(){return c``}}s(t,"styles",[b`
+            a-barbearia-section {
+                --display-botao: none;
+            }
+        `]);customElements.define("a-barbeari-page",t);export{t as A};
